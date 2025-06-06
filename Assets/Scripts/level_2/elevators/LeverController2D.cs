cff79098e113a53cda95f77c6e16835b8b8e88c1
mahdi <m.m.safarri@gmail.com>
2025-06-06 16:44:45 +0330

    add swich position to levers

--- Assets/Scripts/level_2/elevators/LeverController2D.cs
+++ Assets/Scripts/level_2/elevators/LeverController2D.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class LeverController2D : MonoBehaviour
 {
-    public ElevatorController2D elevator; // رفرنس به اسکریپت آسانسور
+    public ElevatorController2D elevator;   // رفرنس به اسکریپت آسانسور
+    public Sprite sprite1;                  // اسپرایت حالت اول
+    public Sprite sprite2;                  // اسپرایت حالت دوم
 
     private bool playerInZone = false;
+    private bool isFirstSprite = true;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite1;
+    }
 
     void Update()
     {
         if (playerInZone && Input.GetKeyDown(KeyCode.P))
         {
+            // تغییر حالت آسانسور
             elevator.ToggleMove();
+
+            // تغییر اسپرایت اهرم
+            isFirstSprite = !isFirstSprite;
+            spriteRenderer.sprite = isFirstSprite ? sprite1 : sprite2;
         }
     }
 
