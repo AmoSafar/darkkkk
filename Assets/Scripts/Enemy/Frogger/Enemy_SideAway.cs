be72d225838c07ed2bbcddb728499ba10b5bcc58
Hosein <208934407+Hoseinafk@users.noreply.github.com>
2025-06-09 16:21:26 +0330

    Bugs Fixed

--- Assets/Scripts/Enemy/Frogger/Enemy_SideAway.cs
+++ Assets/Scripts/Enemy/Frogger/Enemy_SideAway.cs
@@ -49,4 +49,16 @@ public class Enemy_SideAway : MonoBehaviour
             }
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Health playerHealth = collision.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }
+
 }
