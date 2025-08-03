ed46fb7a03daa678b78c82e055ab5873f8d7fcf1
mahdi <m.m.safarri@gmail.com>
2025-08-03 23:48:08 +0330

    players can open the door

--- Assets/Scripts/level_2/door/DoorOpenMap3.cs
+++ Assets/Scripts/level_2/door/DoorOpenMap3.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
-public class DoorOP : MonoBehaviour
+public class DoorOpenMap3 : MonoBehaviour
 {
     private bool isOpen = false;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    
 
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip openSound;
+    
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite1;
+    }
 
     public void OpenDoor()
     {
@@ -20,6 +31,8 @@ public class DoorOP : MonoBehaviour
         {
             audioSource.PlayOneShot(openSound);
         }
+        spriteRenderer.sprite = sprite2;
+
 
         Debug.Log("Door opened by KeyTriggerZone!");
     }
