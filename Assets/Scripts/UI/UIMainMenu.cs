49a72a46620c0b9c9e606c7ce2ff0fb7612b3e85
Hosein <208934407+Hoseinafk@users.noreply.github.com>
2025-08-11 11:59:10 +0330

    Start And Lose Conditions

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -41,7 +41,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1); // بارگذاری صحنه با Index 1
+        SceneManager.LoadScene(2); // بارگذاری صحنه با Index 1
     }
 
     public void QuitGame()
